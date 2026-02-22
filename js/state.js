bb7aa69e7a1ad4577eff382378c86232b65dc5c5
bagus farouk <b46usf@gmail.com>
2026-02-22 16:30:37 +0700

    commit

--- js/state.js
+++ js/state.js
@@ -1,2 +1,34 @@
 let MODE = "individu"; // individu | kelompok | total
 let pieInfChart, pieNonInfChart, lineChart;
+
+tailwind.config = {
+  darkMode: 'class',
+}
+
+/* ===== Dark Mode ===== */
+const toggleBtn = document.getElementById('themeToggle');
+
+if(localStorage.theme === 'dark'){
+  document.documentElement.classList.add('dark');
+}
+
+toggleBtn.onclick = () => {
+  document.documentElement.classList.toggle('dark');
+  localStorage.theme =
+    document.documentElement.classList.contains('dark')
+    ? 'dark' : 'light';
+}
+
+/* ===== Segmented Control ===== */
+function setMode(index){
+  const indicator = document.getElementById('indicator');
+  indicator.style.transform = `translateX(${index*100}%)`;
+}
+
+/* ===== Remove Skeleton After Load ===== */
+window.addEventListener("load",()=>{
+  setTimeout(()=>{
+    document.querySelectorAll('.skeleton')
+      .forEach(el=>el.classList.remove('skeleton'));
+  },1200);
+});
